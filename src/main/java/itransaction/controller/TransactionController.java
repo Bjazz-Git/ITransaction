f9e644e035ff7acf7c3cb13945305d4b97631251
Bjazz <181002693+Bjazz-Git@users.noreply.github.com>
2026-06-03 10:17:17 -0400

    Cleanup

--- src/main/java/itransaction/controller/TransactionController.java
+++ src/main/java/itransaction/controller/TransactionController.java
@@ -3,9 +3,6 @@ package itransaction.controller;
 import itransaction.model.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import itransaction.view.AdminMenu;
-import itransaction.view.CustomerMenu;
-import itransaction.view.RetrieveInput;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -109,41 +106,5 @@ public class TransactionController {
     @DeleteMapping("/accounts/{id}")
     void deleteAccount(@PathVariable String id){
         accountRepository.deleteAccount(id);
-    }
-
-    private static String login(String username, String password) {
-        if (username.equals("admin") && password.equals("admin123")) {
-            return "admin";
-        }
-
-        for (User user : users) {
-            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
-                return user.getUsername();
-            }
-        }
-
-        return "";
-
-    }
-
-    private static Account getAccount(Customer customer) {
-        if (customer != null) {
-            List<Account> accounts = customer.getAccounts();
-
-            for (int i = 0; i < accounts.size(); i++) {
-                System.out.println(accounts.get(i));
-            }
-
-            String id = RetrieveInput.readString("Enter the account's id:");
-
-            for (int i = 0; i < accounts.size(); i++) {
-                Account account = accounts.get(i);
-                if (account.getId().equals(id)) {
-                    return account;
-                }
-            }
-        }
-
-        return null;
     }
 }
